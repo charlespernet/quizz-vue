704c36f295cea18bc6b446d67d90ff189d7c7c8b
Romain Manguin <harlequin.crest@gmail.com>
2020-11-23 15:12:21 +0100

    Removes unused css

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -37,39 +37,4 @@ export default {
 </script>
 
 <style lang='scss' scoped>
-.ranking {
-  background: #ffffff;
-  border-radius: 100px;
-  margin: 10px;
-  position: relative;
-  display: flex;
-  padding: 5px;
-  justify-content: space-between;
-  font-weight: bold;
-  margin-bottom: 22px;
-  &-position {
-    color: white;
-    background: #0c1131;
-    width: 40px;
-    height: 40px;
-    border-radius: 50%;
-    position: absolute;
-    left: 0;
-    top: -3px;
-    border: 4px solid white;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-  }
-  &-text {
-    color: black;
-    margin-left: 55px;
-  }
-  &-score {
-    color: white;
-    background: #0c1131;
-    width: 80px;
-    border-radius: 100px;
-  }
-}
 </style>
