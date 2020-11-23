3988e912e842920a2522d824297ac9645b210647
Charles Pernet <charlespernet@gmail.com>
2020-11-23 14:27:49 +0100

    first working with axios

--- src/components/ranking/Rankings.vue
+++ src/components/ranking/Rankings.vue
@@ -1,24 +1,49 @@
 <template>
   <div>
     <Header />
+    <div v-if="loading" class="loading">Loading...</div>
     <Ranking
       v-for="(ranking, index) in rankings"
-      :key="ranking.score"
+      :key="ranking.fields.Score"
       :position="index + 1"
-      :text="ranking.name"
-      :score="ranking.score"
+      :text="ranking.fields.Name"
+      :score="ranking.fields.Score"
     />
+    <div id="boby">{{ rankings }}</div>
   </div>
 </template>
 
 <script>
 import Header from "../Header.vue";
 import Ranking from "./Ranking.vue";
-import rankings from "../../data/rankings.json";
+import axios from "axios";
+
 export default {
   components: { Header, Ranking },
   data() {
-    return { rankings: rankings };
+    return {
+      loading: false,
+      rankings: [],
+      error: null,
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      this.loading = true;
+      axios({
+        url:
+          "https://api.airtable.com/v0/app4WmGYrPS97vs2O/Results?maxRecords=3&view=Grid%20view",
+        headers: {
+          Authorization: "Bearer keyb9cEx4MPbw7V3x",
+        },
+      }).then((res) => {
+        this.loading = false;
+        this.rankings = res.data.records;
+      });
+    },
   },
 };
 </script>
