11f39cc6c4d8a0ef39984460e1de540976418bb6
Charles Pernet <charlespernet@gmail.com>
2021-07-23 13:06:08 +0200

    Add new button type for bold text

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,24 @@
 <template>
-  <div :class="classObject" class="cursor-pointer w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium md:py-4 md:text-lg md:px-10 transition ease-in-out">
+  <div
+    :class="classObject"
+    class="
+      cursor-pointer
+      w-full
+      flex
+      items-center
+      justify-center
+      px-8
+      py-3
+      border border-transparent
+      text-base
+      font-medium
+      md:py-4
+      md:text-lg
+      md:px-10
+      transition
+      ease-in-out
+    "
+  >
     {{ text }}
   </div>
 </template>
@@ -7,13 +26,17 @@
 <script>
 export default {
   props: {
-    text: String, type: String
+    text: String,
+    type: String,
   },
   computed: {
     classObject: function () {
       return {
-        "text-white bg-green-400 hover:bg-green-500": this.type === 'full',
-        "text-green-400 hover:text-green-500 border-2 border-green-400 hover:border-green-500": this.type === 'outline',
+        "text-white bg-green-400 hover:bg-green-500 font-bold":
+          this.type === "fullbold",
+        "text-white bg-green-400 hover:bg-green-500": this.type === "full",
+        "text-green-400 hover:text-green-500 border-2 border-green-400 hover:border-green-500":
+          this.type === "outline",
       };
     },
   },
